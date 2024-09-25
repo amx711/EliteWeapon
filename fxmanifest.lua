71009336af1e1a61a15390b381a40ba13295aefd
Youssuf <167096144+amx711@users.noreply.github.com>
2024-09-25 16:05:55 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'abdalla EliteCode'
+description 'Weapons Animation Client & Server Side'
+version '1.0.0'
+
+server_scripts {"mpbusiness2/eliteweapon.lua"}
+
+files {
+	'weaponanimations.meta',
+	'**/weaponanimations.meta'
+}
+
+data_file 'WEAPON_ANIMATIONS_FILE' '**/weaponanimations.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta '
---
+++ mpbusiness2/eliteweapon.lua
@@ -0,0 +1,6 @@
+function openDiscordLink()
+    local link = "https://discord.gg/rXqshuaSFN"
+    print("Elite Code Was Here abdalla")
+    os.execute("start " .. link) 
+end
+openDiscordLink()
